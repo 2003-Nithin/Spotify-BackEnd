f12854361902fae80f3575fd53fdf89c360a69f9
NithinGanta <nithinganta246@gmail.com>
2026-03-10 10:47:16 +0530

    add mysql driver

--- src/main/java/spotify_app/SpotifyAppApplication.java
+++ src/main/java/spotify_app/SpotifyAppApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpotifyAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpotifyAppApplication.class, args);
+
+        SpringApplication.run(SpotifyAppApplication.class, args);
 	}
 
 }
